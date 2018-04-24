debf0db0c51fd10c458afe105adbf3aee29856c6
kz56cd <kz56cd@gmail.com>
2018-04-25 06:10:11 +0900

    Add google service plist / Add inserting and observing firebase realtime database logic

--- PreFRDB/ViewController.swift
+++ PreFRDB/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+//    var ref: = Database.database().reference()
+    fileprivate var ref: DatabaseReference?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        prepareFirebase()
+        handleFirebase()
+    }
+}
+
+extension ViewController {
+
+    fileprivate func prepareFirebase() {
+        ref = Database.database().reference()
+        
+        // observe
+        ref?.observe(.value) { snapshot in
+            let postDict = snapshot.value as? [String: Any] ?? [:]
+            if postDict.count > 0 {
+                print("snapshot: \(postDict.debugDescription)")
+            } else {
+                print("snapshot: empty")
+            }
+        }
+
+        ref?.child("users").child("user2").observe(.value) { snapshot in
+            let postDict = snapshot.value as? [String: Any] ?? [:]
+            if postDict.count > 0 {
+                print("snapshot ( users -> user2): \(postDict.debugDescription)")
+            } else {
+                print("snapshot ( users -> user2): empty")
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    fileprivate func handleFirebase() {
+        // insert
+        ref?.child("users").child("user1").setValue(["name": "Alice"])
+        ref?.child("users").child("user2").setValue(["name": "Damo"])
     }
-
-
 }
 
